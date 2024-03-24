52fffe9e5665f862206201ebd8cb3afc65019696
Smirnov <smirnoff.yevhen@gmail.com>
2024-03-24 10:03:41 +0200

    fixed client path

--- api/src/app.js
+++ api/src/app.js
@@ -19,16 +19,14 @@ import { initDriver } from './neo4j.js';
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, '../views'));
-// app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '../../client/build')))
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client/build')))
 
 initDriver(process.env.DB_URL, process.env.USER_NAME, process.env.PASSWORD);
 
@@ -37,7 +35,7 @@ app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/build/index.html'))
+  res.sendFile(path.join(__dirname + '../../client/build/index.html'))
 })
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
