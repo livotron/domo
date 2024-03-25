46d2084cc6a519ef7e984ccc134b8f2593007cd0
Smirnov <smirnoff.yevhen@gmail.com>
2024-03-25 19:45:10 +0200

    added verification endpoint

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,5 @@
 import { getSession } from "../src/neo4j.js";
 
-
 export const getUserByName = async (name, sessionContext) => {
   const session = getSession(sessionContext);
   const checkupQuery = await session.executeWrite((tx) => {
@@ -9,9 +8,10 @@ export const getUserByName = async (name, sessionContext) => {
     });
   });
   if (!checkupQuery.records.length) {
-    return null
-  }  return checkupQuery.records[0].get('user').properties
-}
+    return null;
+  }
+  return checkupQuery.records[0].get("user").properties;
+};
 
 export const mergeUser = async (name, password, sessionContext) => {
   const session = getSession(sessionContext);
@@ -26,5 +26,139 @@ export const mergeUser = async (name, password, sessionContext) => {
     );
   });
 
-  return query.records[0].get('user').properties
-}
+  return query.records[0].get("user").properties;
+};
+
+const getOppositeDirection = (direction) => {
+  switch (direction) {
+    case "UP":
+      return "DOWN";
+    case "RIGHT":
+      return "LEFT";
+    case "DOWN":
+      return "UP";
+    case "LEFT":
+      return "RIGHT";
+  }
+};
+
+export const verifyPartner = async (
+  userName,
+  partnerName,
+  direction,
+  hash,
+  sessionContext
+) => {
+  const session = getSession(sessionContext);
+
+  await session.executeWrite((tx) => {
+    return tx.run(
+      `MATCH (:User {name: $userName})-[v:VERIFIES{direction: $direction}]->()
+      DELETE v`,
+      {
+        userName,
+        direction,
+      }
+    );
+  });
+
+  await session.executeWrite((tx) => {
+    return tx.run(
+      `MATCH (:User {name: $userName})-[v:SUPPORTS{direction: $direction}]->()
+      DELETE v`,
+      {
+        userName,
+        direction,
+      }
+    );
+  });
+
+  await session.executeWrite((tx) => {
+    return tx.run(
+      `MATCH (:User {name: $userName})<-[v:SUPPORTS{direction: $oppositeDirection}]-()
+      DELETE v`,
+      {
+        userName,
+        oppositeDirection: getOppositeDirection(direction),
+      }
+    );
+  });
+
+  const partnerRecord = await session.executeRead((tx) => {
+    return tx.run(
+      `MATCH (u:User {name: $partnerName})
+      RETURN u
+      `,
+      {
+        partnerName,
+      }
+    );
+  });
+
+  if (partnerRecord.records.length) {
+    await session.executeWrite((tx) => {
+      return tx.run(
+        `MATCH (u:User {name: $userName}), (p:User {name: $partnerName})
+      MERGE (u)-[v:VERIFIES{direction: $direction, hash: $hash}]->(p)
+      RETURN v
+      `,
+        {
+          userName,
+          partnerName,
+          direction,
+          hash,
+        }
+      );
+    });
+  
+    const verifiedMe = await session.executeRead((tx) => {
+      return tx.run(
+        `MATCH (u:User {name: $userName})<-[:VERIFIES{direction: $oppositeDirection, hash: $hash}]-(:User {name: $partnerName})
+        RETURN u
+        `,
+        {
+          userName,
+          partnerName,
+          oppositeDirection: getOppositeDirection(direction),
+          hash,
+        }
+      );
+    });
+  
+    if (verifiedMe.records.length) {
+      const supportRelations = await session.executeWrite((tx) => {
+        return tx.run(
+          `
+          MATCH (u:User {name: $userName}), (p:User {name: $partnerName})
+          CREATE (u)-[r:SUPPORTS{direction: $direction}]->(p),
+          (u)<-[t:SUPPORTS{direction: $oppositeDirection}]-(p)
+          RETURN r,t
+          `,
+          {
+            userName,
+            partnerName,
+            direction,
+            oppositeDirection: getOppositeDirection(direction),
+          }
+        );
+      });
+    }
+  
+  } else {
+    await session.executeWrite((tx) => {
+      return tx.run(
+        `MATCH (u:User {name: $userName})
+      MERGE (u)-[v:VERIFIES{direction: $direction, hash: $hash}]->(:User {name: $partnerName})
+      RETURN v
+      `,
+        {
+          userName,
+          partnerName,
+          direction,
+          hash,
+        }
+      );
+    });
+  }
+  return;
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { getSession } from "../src/neo4j.js";
 import auth from "../src/auth.js";
-import { getUserByName, mergeUser } from "../models/user.js";
+import { getUserByName, mergeUser, verifyPartner } from "../models/user.js";
 
 var router = express.Router();
 
@@ -21,8 +21,12 @@ router.post("/register", async function (req, res, next) {
       });
     }
 
-    const registeredUser = await mergeUser(req.body.name, req.body.password, req);
-    const {password, ...passwordlessUser} = registeredUser
+    const registeredUser = await mergeUser(
+      req.body.name,
+      req.body.password,
+      req
+    );
+    const { password, ...passwordlessUser } = registeredUser;
     res.send(passwordlessUser);
   } catch (e) {
     res.status(500).send({
@@ -68,16 +72,33 @@ router.post("/login", async function (req, res, next) {
   }
 });
 
-router.get('/me', auth , async function (req,res) {
+router.get("/me", auth, async function (req, res) {
   try {
-    const user = await getUserByName(req.user.name, req)
+    const user = await getUserByName(req.user.name, req);
 
-    return res.status(200).send(user)
-  } catch(e) {
+    return res.status(200).send(user);
+  } catch (e) {
     res.status(500).send({
-      message: e.message
-    })
+      message: e.message,
+    });
   }
-})
+});
+
+router.post("/verify", auth, async function (req, res) {
+  try {
+    const verify = await verifyPartner(
+      req.user.name,
+      req.body.partnerName,
+      req.body.direction,
+      req.body.hash,
+      req
+    );
+    res.send(verify)
+  } catch (e) {
+    res.status(500).send({
+      message: e.message,
+    });
+  }
+});
 
 export default router;
