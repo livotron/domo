1733b439b2500ac390378253978a584e05fe7fce
Smirnov <smirnoff.yevhen@gmail.com>
2024-04-02 21:43:01 +0300

    fixed user navigation

--- client/src/features/user/DisplayRelationsPage.tsx
+++ client/src/features/user/DisplayRelationsPage.tsx
@@ -1,26 +1,39 @@
 import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import { DisplayPartners } from "./DisplayPartners";
 import { useAppDispatch } from "app/store";
-import { fetchByName, fetchMe, receiveUser, toggleIsFixed } from "./userSlice";
+import { fetchByName, fetchMe, fetchPartners, receiveUser, toggleIsFixed } from "./userSlice";
 import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "app/rootReducer";
 import { FixedRelationsMenu } from "./FixedRelationsMenu";
 import { Login } from "./Login";
 import { SearchUser } from "./SearchUser";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const DisplayRelationsPage = () => {
   const dispatch = useAppDispatch();
-
+  const navigate = useNavigate();
   const me = useSelector((state: RootState) => state.user.me);
   const centralUser = useSelector((state: RootState) => state.user.user);
   const handleSearch = (searchedUser: string | null) => {
-    if (searchedUser) dispatch(fetchByName(searchedUser));
+    if (searchedUser) {
+      // dispatch(fetchByName(searchedUser));
+      navigate(`/comrades/${searchedUser}`);
+    }
   };
+  const { name } = useParams();
 
+  useEffect(() => {
+    if (name) {
+      console.log("navigating by params");
+      dispatch(receiveUser({ name }));
+      dispatch(fetchPartners());
+    }
+  }, [name]);
   const handleMeClicked = (e: MouseEvent) => {
     e.preventDefault();
-    dispatch(receiveUser(me));
+    // dispatch(receiveUser(me));
+    navigate(`/comrades/${me.name}`);
   };
   return (
     <>
