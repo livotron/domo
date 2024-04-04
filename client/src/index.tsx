d9dd1c142dcb0be563c9fe6614a8423ebfe4b771
Smirnov <smirnoff.yevhen@gmail.com>
2024-04-04 07:59:11 +0300

    Added theme provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import { BrowserRouter } from 'react-router-dom';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app/App";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import { BrowserRouter } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    // primary: {
+    //   main: "#000000",
+    //   contrastText: "#fff",
+    // },
+    // secondary: {
+    //   main: "#13b6fc",
+    //   contrastText: "#000",
+    // },
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>
 );
-
