7efc2279bdf044acc5608ead3ba9cd0b25c3cb86
Smirnov <smirnoff.yevhen@gmail.com>
2024-04-06 19:01:47 +0300

    added getClaims and acknowledge endpoints

--- api/models/claims.js
+++ api/models/claims.js
@@ -2,6 +2,21 @@ import neo4j from "neo4j-driver";
 import { getSession } from "../src/neo4j.js";
 import { DateTime } from "luxon";
 
+const getReadDelta = (level) => {
+  const deltas = [
+    null,
+    { hours: 20 },
+    { hours: 10 },
+    { hours: 5 },
+    { hours: 2 },
+    { hours: 1 },
+    { minutes: 30 },
+    { minutes: 15 },
+    { minutes: 5 },
+  ];
+  return deltas[level];
+};
+
 export const createClaim = async (name, title, text, level, sessionContext) => {
   const session = getSession(sessionContext);
   const dateNow = DateTime.utc();
@@ -19,7 +34,7 @@ export const createClaim = async (name, title, text, level, sessionContext) => {
     );
   });
   const properties = createdClaim.records[0].get("claim").properties;
-  return properties
+  return properties;
 };
 
 export const deleteUserFocus = async (userName, sessionContext) => {
@@ -33,38 +48,99 @@ export const deleteUserFocus = async (userName, sessionContext) => {
   });
 };
 
+export const getUserFocus = async (userName, sessionContext) => {
+  const session = getSession(sessionContext);
+  const dive = await session.executeRead((tx) => {
+    return tx.run(
+      `MATCH (u:User {name: $name})-[f:FOCUSES]->(k:Dive)
+      Return k as dive`,
+      { name: userName }
+    );
+  });
+  return dive.records[0].get("dive").properties;
+};
+
 export const startNewDive = async (userName, sessionContext) => {
   const session = getSession(sessionContext);
+  const previousDive = await getUserFocus(userName, sessionContext);
+
   await deleteUserFocus(userName, sessionContext);
 
   const dateNow = DateTime.utc();
-  const dateDayAgo = dateNow.minus({ days: 1 });
+  const dateCurrent = dateNow.minus(getReadDelta(1));
+  const datePrevious = DateTime.fromISO(previousDive.createdAt).setZone('utc');
+  console.log(dateCurrent, datePrevious);
+  const finalDate = dateCurrent > datePrevious ? dateCurrent : datePrevious;
   const newDive = await session.executeWrite((tx) => {
     return tx.run(
       `
       MATCH (u:User {name: $name})
-      CREATE (u)-[:INITIATES]->(d: Dive {level: 1, createdAt: $createdAt, reached: $reached})
+      CREATE (u)-[:INITIATES]->(d:Dive {level: 1, createdAt: $createdAt, stopAt: $stopAt, acknowledgmentLogs: []})
       CREATE (u)-[:FOCUSES]->(d)
       RETURN d as dive
       `,
       {
         name: userName,
         createdAt: dateNow.toString(),
-        reached: dateDayAgo.toString(),
+        stopAt: finalDate.toString(),
       }
     );
   });
   const properties = newDive.records[0].get("dive").properties;
-  return {
-    ...properties,
-  };
+  return properties;
+};
+
+export const getClaims = async (userName, sessionContext) => {
+  const session = getSession(sessionContext);
+
+  const claims = await session.executeRead((tx) => {
+    return tx.run(
+      `
+      MATCH (u:User {name: $name})-[:FOCUSES]->(d:Dive)
+      WITH d.createdAt as from, d.stopAt as to, d.level as level
+      MATCH (c:Claim)
+      WHERE c.level = level AND c.createdAt < from AND c.createdAt > to
+      return c as claims
+      ORDER BY c.createdAt ASC
+      `,
+      {
+        name: userName,
+      }
+    );
+  });
+  console.log(claims.records);
+  const properties = claims.records.map((rec) => rec.get("claims").properties);
+  return properties;
+};
+
+export const acknowlegeClaim = async (
+  userName,
+  creationTime,
+  creatorName,
+  sessionContext
+) => {
+  const session = getSession(sessionContext);
+
+  const claims = await session.executeWrite((tx) => {
+    return tx.run(
+      `
+      MATCH (u:User {name: $name})-[:FOCUSES]->(d:Dive)
+      SET d.stopAt = $creationTime
+      SET d.acknowledgmentLogs = d.acknowledgmentLogs + $logs
+      `,
+      {
+        name: userName,
+        creationTime,
+        logs: JSON.stringify({ name: creatorName, time: creationTime }),
+      }
+    );
+  });
+  return null;
 };
 
 export const matchClaim = async (userName, createdAt, sessionContext) => {
   const session = getSession(sessionContext);
-  // const dateTime = DateTime.fromISO(createdAt)
-
-  const newClaim = await session.executeWrite((tx) => {
+  const newClaim = await session.executeRead((tx) => {
     return tx.run(
       `MATCH (u:User {name: $name})-[:WRITES]->(c:Claim {createdAt: $createdAt})
       RETURN c as claim`,
@@ -74,8 +150,8 @@ export const matchClaim = async (userName, createdAt, sessionContext) => {
       }
     );
   });
-  const properties = newDive.records[0].get("claim");
-  console.log(properties);
+  if (!newClaim.records.length) return null;
+  const properties = newClaim.records[0].get("claim").properties;
   return properties;
 };
-//"createdAt": "2024-04-05T08:56:57.808Z"
+
--- api/routes/claims.js
+++ api/routes/claims.js
@@ -1,6 +1,12 @@
 import express from "express";
 import auth from "../src/auth.js";
-import { createClaim, matchClaim, startNewDive } from "../models/claims.js";
+import {
+  acknowlegeClaim,
+  createClaim,
+  getClaims,
+  matchClaim,
+  startNewDive,
+} from "../models/claims.js";
 
 var router = express.Router();
 
@@ -21,20 +27,28 @@ router.post("/", auth, async function (req, res) {
 });
 
 router.post("/new-dive", auth, async function (req, res) {
-  const createdDive = await startNewDive(
+  const createdDive = await startNewDive(req.user.name, req);
+  res.send(createdDive);
+});
+
+router.post("/acknowledge", auth, async function (req, res) {
+  const createdDive = await acknowlegeClaim(
     req.user.name,
+    req.body.creationTime,
+    req.body.creatorName,
     req
   );
   res.send(createdDive);
 });
 
 router.get("/by-time-and-user/:time/:name", auth, async function (req, res) {
-  const declaration = await matchClaim(
-    req.params.name,
-    req.params.time,
-    req
-  );
-  res.send(declaration);
+  const claim = await matchClaim(req.params.name, req.params.time, req);
+  res.send(claim);
+});
+
+router.get("/get-claims", auth, async function (req, res) {
+  const claim = await getClaims(req.user.name, req);
+  res.send(claim);
 });
 
 export default router;
